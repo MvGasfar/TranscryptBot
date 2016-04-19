da58d79ef0f3e35e61a352e8898e9b8dca56f3b8
MvGasfar <mv.gasfar@gmail.com>
2016-04-19 19:08:31 +0300

    Adding projects

---
+++ TranscryptBot/TranscryptApiManager/TranscryptApiConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranscryptApiSoapLayer;
+
+namespace TranscryptApiManager
+{
+    public class TranscryptApiConnector
+    {
+		private readonly ExtendedTranscryptServiceMethods _extendedTranscryptApi
+			= new ExtendedTranscryptServiceMethods("TranscryptServiceApiBinding", "TranscryptServiceApi");
+		public ExtendedTranscryptServiceMethods ExtendedTranscryptApi { get { return _extendedTranscryptApi; } }
+	}
+}
---
+++ TranscryptBot/TranscryptBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranscryptBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
